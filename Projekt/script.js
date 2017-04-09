a17bac4fde99c9dc02c7a3c330c1028fdb2b340c
Drizzet <dawid.bogacki@gmail.com>
2017-04-09 11:32:02 +0200

    script - date

---
+++ Projekt/script.js
@@ -0,0 +1,4 @@
+var d = new Date();
+var months = ['stycznia', 'lutego', 'marca', 'kwietnia', 'maja', 'czerwca', 'lipca', 'sierpnia', 'wrzesnia', 'pazdziernika', 'listopada', 'grudnia'];
+var dt = document.getElementById('datetime');
+dt.innerHTML = d.getDate() + ' ' + months[d.getMonth()] + ' ' + d.getFullYear() + 'r.'; 
