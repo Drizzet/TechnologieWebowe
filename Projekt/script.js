7397bebae035b50b782d6ed90257493e4d6e46ef
Drizzet <dawid.bogacki@gmail.com>
2017-04-25 10:48:34 +0200

    style -poprawka 1/2
    jeszcze bez rytmu pionowego

--- Projekt/script.js
+++ Projekt/script.js
@@ -2,3 +2,5 @@ var d = new Date();
 var months = ['stycznia', 'lutego', 'marca', 'kwietnia', 'maja', 'czerwca', 'lipca', 'sierpnia', 'wrzesnia', 'pazdziernika', 'listopada', 'grudnia'];
 var dt = document.getElementById('datetime');
 dt.innerHTML = d.getDate() + ' ' + months[d.getMonth()] + ' ' + d.getFullYear() + 'r.'; 
+
+var nw = document.getElementById('intext').value;
