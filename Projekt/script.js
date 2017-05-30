713498285a1896e41fca8db5941e57b3756b810d
Drizzet <dawid.bogacki@gmail.com>
2017-05-30 10:57:28 +0200

    faza IV

--- Projekt/script.js
+++ Projekt/script.js
@@ -13,16 +13,36 @@ function reload(){
 	while(list.firstChild){						//usuwanie listy
 		list.removeChild(list.firstChild);
 	}
-	if(tabLength == 0){							//lista jest pusta
-		var newElement = document.createElement('h4');
-		newElement.innerHTML = "Nie zaplanowałeś żadnych zadań";
-		list.appendChild(newElement);
+	if(tabLength == 0){							//tablica jest pusta
+		var pusto = document.createElement('h4');
+		pusto.innerHTML = "Nie zaplanowałeś żadnych zadań";
+		list.appendChild(pusto);
 	}
 	else{										//ładowanie niepustej listy
 	for (var i=0; i<tabLength; i++){
-			var newElement = document.createElement('li');
-			newElement.innerHTML = tabList[i];
-			list.appendChild(newElement);
+			var newElement = document.createElement('li');			// Utworzenie nowego elementu i przechowywanie go w zmiennej.
+			var newText = document.createTextNode(tabList[i].tekst);	// Utworzenie węzła tekstowego i przechowywanie go w zmiennej.
+
+			var position = list;										// Wyszukanie miejsca, w którym powinien być dodany nowy element.
+			
+			var	newButton = document.createElement('button');			//przycisk 'usun'
+			newButton.setAttribute('class', 'delete'); 
+			newButton.textContent = 'Usuń';
+			newButton.setAttribute('id', i);
+			newButton.onclick = del;
+			
+			var	newCheck = document.createElement('input');			//checkbox	
+			newCheck.type = 'checkbox';
+			newCheck.setAttribute('class', 'check');
+			if(tabList[i].check == 1){
+				newCheck.checked = 'checked';
+			}
+			newCheck.onclick = check;
+			
+			newElement.appendChild(newCheck);
+			newElement.appendChild(newText);							// Dołączenie nowego węzła tekstowego do nowego elementu.
+			newElement.appendChild(newButton);			
+			position.appendChild(newElement);							// Wstawienie nowego elementu we wskazanym miejscu.
 		}
 	}
 }
@@ -34,19 +54,35 @@ function add(){
 		alert("Nie mozesz dodac pustego zadania");	//kontrola pustego zadania
 	}
 	else{
-		tabList[tabLength] = '<input type="checkbox" class="check"  id="task' +tabLength+ '" onclick="check()" /> <label>' +input+' ' + '</label><input type="button" class="delete" value="usuń" id="btask' +tabLength+'" onclick="del()" />';
+		tabList[tabLength] = { 	tekst: input,
+								check: 0};
 		tabLength++;
 	}
 	reload();
 }
 
 function del(){
-
+	for(var a = this.id;a <= tabLength-1;a++)
+	{
+		tabList[a]=tabList[a+1];
+	}
+	tabLength--;
 	reload();
 }
 
 function check(){
-
+	if(tabList[this.id].check==1){
+	tabList[this.id].check=0;
+	}else{
+	tabList[this.id].check=1;}
+	reload();
 }
-
+function keyEnter(event){
+	console.log(event.keyCode);
+	if(event.keyCode==13){
+		add();
+	}
+}
+document.onkeydown = keyEnter;
 document.onload = reload();
+//'<input type="checkbox" class="check"  id="task' +tabLength+ '" onclick="check()" /> <label>' +input+' ' + '</label><input type="button" class="delete" value="usuń" id="btask' +tabLength+'" onclick="del()" />';
