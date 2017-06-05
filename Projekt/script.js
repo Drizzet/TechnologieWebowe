dbd286f008c25ab1489170fea970d336c945ca8d
Drizzet <dawid.bogacki@gmail.com>
2017-06-05 18:49:53 +0200

    faza V
    nie działa coś z dodawaniem elementu, wyglada jakby API sie nie
    zaupdetjtowalo

--- Projekt/script.js
+++ Projekt/script.js
@@ -9,6 +9,7 @@ var list = document.getElementById('list');
 var url='http://sealcode.org:8082/api/v1/resources/task';		//API sealcodowe
 
 function reload(){
+	console.log('refreszuje, tabLength: ',tabLength);
 	document.getElementById("intext").value = '';	//czyszczenie pola do wpisywania zadania
 
 	while(list.firstChild){						//usuwanie listy
@@ -22,8 +23,8 @@ function reload(){
 	else{										//ładowanie niepustej listy
 	for (var i=0; i<tabLength; i++){
 			var newElement = document.createElement('li');			// Utworzenie nowego elementu i przechowywanie go w zmiennej.
-			var newText = document.createTextNode(tabList[i].tekst);	// Utworzenie węzła tekstowego i przechowywanie go w zmiennej.
-
+			var newText = document.createTextNode(tabList[i].body.title);	// Utworzenie węzła tekstowego i przechowywanie go w zmiennej.
+			console.log('dodaje do listy ',newText);
 			var position = list;										// Wyszukanie miejsca, w którym powinien być dodany nowy element.
 			
 			var	newButton = document.createElement('button');			//przycisk 'usun'
@@ -35,7 +36,7 @@ function reload(){
 			var	newCheck = document.createElement('input');			//checkbox	
 			newCheck.type = 'checkbox';
 			newCheck.setAttribute('class', 'check');
-			if(tabList[i].check == true){
+			if(tabList[i].body.is_done == true){
 				newCheck.checked = 'checked';
 			}
 			newCheck.setAttribute('id', i);
@@ -47,6 +48,7 @@ function reload(){
 			position.appendChild(newElement);							// Wstawienie nowego elementu we wskazanym miejscu.
 		}
 	}
+	
 }
 
 function add(){
@@ -56,33 +58,80 @@ function add(){
 		alert("Nie mozesz dodac pustego zadania");	//kontrola pustego zadania
 	}
 	else{
-		tabList[tabLength] = { 	tekst: input,
-								check: false};
-		tabLength++;
+		
+		tabList[tabLength] = { 	title: input,
+								is_done: false};
+		//tabLength++;
+	
+	qwest.post(url, {title: tabList[tabLength].title, is_done: tabList[tabLength].is_done}, {cache: true});
 	}
-	reload();
+	console.log('dodaje taska, tabLength: ',tabLength);
+	getTasks();
+	//reload();
 }
 
 function del(){
-	tabList.splice(this.id,1);
-	tabLength--;
-	reload();
+	//tabList.splice(this.id,1);
+	qwest.delete(url+'/'+tabList[this.id].id, null, {cache: true}).then(function(xhr, response){
+	getTasks();})
+	//reload();})
+	//tabLength--;
 }
 
 function check(){
 	
-	if(tabList[this.id].check==true){
+	/*if(tabList[this.id].check==true){
 	tabList[this.id].check=false;
 	}else{
-	tabList[this.id].check=true;}
-	reload();
+	tabList[this.id].check=true;}*/
+	tabList[this.id].body.is_done = this.checked;
+	qwest.map('PATCH', url+'/'+tabList[this.id].id, tabList[this.id].body, {cache: true}).then(function(xhr, response) { // szukamy odpowiedniego zasobu na serwerze i modyfikujemy jego ciało
+		getTasks(); // odświeżamy stan strony
+	});
 }
 function keyEnter(event){
-	console.log(event.keyCode);
+	//console.log(event.keyCode);
 	if(event.keyCode==13){
 		add();
 	}
 }
+
 document.onkeydown = keyEnter;
-document.onload = reload();
-//'<input type="checkbox" class="check"  id="task' +tabLength+ '" onclick="check()" /> <label>' +input+' ' + '</label><input type="button" class="delete" value="usuń" id="btask' +tabLength+'" onclick="del()" />';
+document.onload = getTasks();
+
+//Funkcje API -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//pobieranie danych
+function getTasks() { // pobieramy listę zadań po wystąpieniu odpowiedniego zdarzenia
+	
+	tabLength = 0;
+	qwest.get(url, {}, {cache: true}).then(
+		function(xhr, response) {
+			response.forEach(function(element) { // wywołujemy dla każdego pobranego zasobu
+				tabList.push(element);//tasks.push(element); // dodajemy pobrany element zasobu do tablicy "tasks"
+				/* Teraz treść danego zadania i jego inne własciwości będą ukrywać się w tasks[index].body.nazwaWlasciwosci, np. tasks[0].body.title - nazwa pierwszego zadania w tablicy! */
+				tabLength++;
+			console.log('getuje task');
+				 reload();// odświeżamy stan strony
+	})});
+}
+/*
+//wysylanie zasobu
+function addTaskServer(task) { // wysyłamy nowe zadanie po wciśnięciu klawisza ENTER lub kliknięciu przycisku
+	qwest.post(url, {title: task.title, is_done: task.is_done}, {cache: true}); // wysłanie nowego zadania w postaci obiektu o właściwościach "title" i "is_done"
+}
+
+//czesciowa aktualizacja zasobu
+function checkboxClick(event) { // stan kliknięcia checkboxa przy danym zadaniu (załóżmy, że funkcja wywołuje się po wystąpieniu pewnego zdarzenia
+	tasks[this.id].body.is_done = this.checked; // zmiana stanu kliknięcia danego zadania w tablicy (zakładamy, że każde zadanie ma swój identyfikator, dla uproszczenia przyjąłem, że identyfikatorem jest pozycja w tablicy
+	qwest.map('PATCH', url+'/'+tasks[this.id].id, tasks[this.id].body, {cache: true}).then(function(xhr, response) { // szukamy odpowiedniego zasobu na serwerze i modyfikujemy jego ciało
+		refresh(); // odświeżamy stan strony
+	});
+}
+
+//usuwanie zasobu
+function deleteTask() { // usuwanie wybranego zadania pod wpływem wystąpienia pewnego zdarzenia
+	qwest.delete(url+'/'+tasks[this.id].id, null, {cache: true}).then(function(xhr, response) { // usuwamy zadanie o danym identyfikatorze (tym razem nie musimy przesyłać ciała takiego zadania)
+		refresh(); // odświeżamy stan strony
+	});
+}*/
